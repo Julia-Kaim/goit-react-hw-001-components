c1feb8d504f860fc34d55051f8865a22e130ff1e
Julia-Kaim <julia.kaim08@gmail.com>
2023-05-10 19:01:24 +0200

    components added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,20 @@
+import { Profile } from './Profile';
+import user from '../configs/user';
+
+import { Statistics } from './Statistics';
+import data from '../configs/data';
+
 export const App = () => {
   return (
-//     <div class="profile">
-//   <div class="description">
-//     <img
-//       src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-//       alt="User avatar"
-//       class="avatar"
-//     />
-//     <p class="name">Petra Marica</p>
-//     <p class="tag">@pmarica</p>
-//     <p class="location">Salvador, Brasil</p>
-//   </div>
-
-//   <ul class="stats">
-//     <li>
-//       <span class="label">Followers</span>
-//       <span class="quantity">1000</span>
-//     </li>
-//     <li>
-//       <span class="label">Views</span>
-//       <span class="quantity">2000</span>
-//     </li>
-//     <li>
-//       <span class="label">Likes</span>
-//       <span class="quantity">3000</span>
-//     </li>
-//   </ul>
-// </div>
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </Container>
   );
 };
